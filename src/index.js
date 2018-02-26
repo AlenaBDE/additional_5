abe088c5ad940705c0c475ca1517c6299612b447
AlenaBDE <bde.alena@gmail.com>
2018-02-26 15:08:11 +0300

    'solutions5'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+        var i, j, k;
+        var a, b;
+
+        if (str.length % 2 > 0) return false;
+        for (i = 0; i < bracketsConfig.length; i++) {
+            a = str.split(bracketsConfig[i][0]).length;
+            b = str.split(bracketsConfig[i][1]).length;
+            if (a != b) return false;
+        }
+
+        return true;
+};
